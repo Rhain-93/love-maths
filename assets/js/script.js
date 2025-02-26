8d10a81accf34951e5cc61c185ec6bb9eac5b81a
Rhain-U <rhainu@hotmail.co.uk>
2025-02-26 11:33:22 +0000

    Readded two random numbers to the run game function as they disappeared.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,27 @@
-function runGame () {
+//wait for the DOM to finish loading before running the game
+//Get the button elements and add event listeners to them
 
+document.addEventListener("DOMContentLoaded", function () {
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttributeByTagName(data - type) === "submit") {
+        alert("You clicked submit");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        alert(`You clicked ${gameType}`);
+      }
+    });
+  }
+});
+
+function runGame() {
+  let num1 = Math.floor(Math.random() * 25) + 1;
+  let num2 = Math.floor(Math.random() * 25) + 1;  
 }
 
-function checkAnswer () {
+function checkAnswer() {}
 
-}
-
-function calculateCorrectAnswer () {
-
-}
+function calculateCorrectAnswer() {}
 
 function incrementWrongAnswer() {}
 
